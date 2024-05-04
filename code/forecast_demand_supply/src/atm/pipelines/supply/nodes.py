79a095c8ed76103bf9ac5c9122c60121ac9465fa
KrishnenduGhorui <77465776+KrishnenduGhorui@users.noreply.github.com>
2024-05-04 10:12:16 -0700

    Update nodes.py

--- code/forecast_demand_supply/src/atm/pipelines/supply/nodes.py
+++ code/forecast_demand_supply/src/atm/pipelines/supply/nodes.py
@@ -1,1 +1,157 @@
+from typing import Any, Dict
+inport pandas as pd
+import numpy as np
+import math
+from google.cloud import bigquery
+from sklearn import preprocessing
+import matplotlib.pyplot as plt
+from sklearn.linear_ model import LogisticRegression
+import matplotlib.dates as mdates
+from statsmodels. tsa. exponential_smoothing.ets import ETSModel
+irom dateutil.relativedel ta import relativedelta
+from datetime import datetime
+from datetime inport timedelta
+from joblib import Parallel, delayed
+from hyperopt inport STATUS_OK, Trials, fmin, tpe
+from fbprophet import Prophet
+from hyperopt import hp
+from hyperopt.pyll import scope
+from fbprophet.diagnostics import cross_validation, performance_metrics
+from .demand.
+model_train import tune_model_ets,tune modelsarima, param_tuning tes, mape, tune_model_prophet
+from ..demand.model_train import train_model_ets,train_model_sarimax, train_model_tes
+from.demand.model_train import
+prep_data_stack, tune_model_stack,train_model_stack, prep_data_stack_1_period, forecast_stack, train_model_prophet
+from..demand.etl import sql table_mapping, preparesq1_strings
+from sklearn.metrics import me an_absolute_percentage_error
+import collections
+inport pandas_gbą
+import logging
+
+def last_day_month(run_date):
+    date_obj = datetime.strptime (run_date, "%Y-%m-%d")
+    first_day_next month = date_obj.replace(day=1)+ timedelta (days=32)
+    last_day_current_month = first_day_next month.replace(day=1)- timedelta(days=1)
+    date_str = last_day_current month. strftime("%Y-%m-%d")
+    rpt_dt = pd.to_datetime(date str).date()
+    return rpt_dt
+  
+def load supply_data_queue (df, key_arg queue)
+    '''
+    input param: ba project
+    outputdata frame with bgco histonic AHT,OCC, SHR
+    This function will will load data from big query tables
+    '''
+    bgco_calls=df ['call_answer_dt','rule_type',key_arg_queue]]
+    bgco_calls['call_answer_dt']-pd.to_datetime(bgco_calls['call_answen_dt'])
+    bgco calls-bgco_calls.sort_values(by=['rule_type','call_answer_dt'))
+    bgco_calls.rename(columns={key_arg_queue:'value'},inplace=True)
+    bgco_calls.dropna(inplace=True)
+    bgco_calls.set_index('call_answerdt', inplace=True)
+                          
+    return bgcocalls
+
+def train_test_split(df, horizon):
+  '''
+    Split whole data in trałn and test
+  '''
+  logging. info('Splitting supply data inte tratn and test tata is started' )
+  df.set_index(pd.to_đatetime(df['call month']), inplace=True)
+  df=df.sort_intex()
+  df_train_bgco=df. iloc[:-pd.to_numeric(heriton), :] # Train set
+  dt_test_bgco=df.iloc[-pd.t0_numeric(horizon):,: )# Test set for val tdotion
+  
+  df_train_bgco.index =pd.date_range(start=df_train_bgco.index[0]) , periods=len(df_train_bgco), freq="MS")
+  df_test_bgco.index=pd.date_range(start=df_test_bgeo.indext[0]), perlods=len(df_test_bgco) , freq="MS")
+  
+  logging. info('Spitting data into traln and test data is finished')
+  return dt_train_bgco, df_test_bgco
+
+def train_forecast_models(forecast_input, rule_type, train_start_ date, train_end_date, test_start_date, test_end_date, params_ets, params_sarima, params_tes,params_grid_prophet, supply_model):
+  '''
+  a wrapper to train prophet forecasting model by rule type,
+  and select & save the best hyper<parameters
+  Args:
+      forecast input (datafrane) time series data of historical calls by rule type / date
+      train end date (date): train end date of whole timeseries dataset
+  Returns
+      best params_ dict (aict): best hyper parameter input per rule_type based on past train
+  '''
+  best_params_dict={}
+
+  returned_list=Parallel(njobs=-1) 
+  (delayed(train_forecast_model_rule_type)
+   (selected rule,forecast_input, train_start_date, train_end_date, test_start_date, test_end_date, params_ets, params_sarima, params_tes, params_grid_prophet, supply_model)
+    for selected_rule in rule_type)
+  
+   for selected_rule, model_ result_list in returned_list:
+      if selected_rule not in best_params_dict:
+          best_params_dict[selected_rule]={} 
+      best_params_dict[selected_rule]=model_result_list
+     
+   return best_params_dict
+
+def train_forecast_model_rule_type(selected_rule_type, forecast_input, train_start_date, train_end_date,test_start_date,test_end_date, params_ets,params_sarima, params_tes, params_grid_prophet, supply_model) :
+    '''
+    train the model by rule type, and select the best hyper parameters
+    Args:
+        selected_rule type: selected rule type
+        forecast_input (dataframe): time-series data of historical demand by rule type
+        parameters(dictionary)
+    Returns:
+        selected_rule_type: selected rule type
+        best_params (dict): best hyper parameter input
+    '''
+    df_temp=forecast_select_data(forecast_input, selected_rule_type)
+    model_result_list=Parallel(n_jobs =-1)(
+                      delayed (fitted_model_return) 
+                      (selected_model, df_temp,train_start_date, train_end_date, test_start_date,
+                       test_end_date, params_ets, params_sarima, params_tes, params_grid_prophet)
+                       for selected_model in supply_model)
+  
+    return selected_rule_type, model_result_list
+  
+def fitted_model_return (model_selected, forecast_input, train_start_date, train_end_date, test_start_date, test_end_date, params_ets, params_sarima,params_tes, params_ grid prophet):
+    '''
+    Perform model training , hyper parmeter tuning , provide best paremters
+    Input :
+          model name: string,
+          data : dataframe,
+          parameters : dictionary
+    Output : model name, best trained model object, best paramters
+    '''
+    train_start_date=pd.to_datetime(train_start_date)
+    train_end_date=pd.to_datetime(train_end_date)
+    test_start_date=pd.to_datetime(test start_date)
+    test_end_date=pd.to_datetime (test_end_date)
+    data_train=forecast_ input.loc[ (forecast_ input.index >= train_start_date) & (forecast_input. index <= train_end_date) ]
+    data_test=forecast_input. loc[ (forecast_input. index >= test_start_date) & (forecast_input. index <= test_end_date) ]
+    
+    if model_selected=='ETS:
+        param grid ets-params_ets
+        best_model_ets, best_params_ets=tune model _ets (data train, data_test, param_grid_ets)
+        return 'ETS' ,best_model_ets, best_params_ets
+
+    elif model_selected=='SARIMAX:
+        param grid_sarima=params_sarima
+        best_model_sarima, best params_sarima-tune_model sarima (data train, data_test, param grid_sarima)
+        return 'SARIMA', best_model_sarima, best_params sarima
+
+    elif model_ selected=='TES':
+        param grid_tes=params_tes
+        best_model tes, best params_tes-param_ tuning tes(data train, data_test, param_grid_tes)
+        return TES,best_model_tes, best_params_tes
+    elif model_selected=='PROPHET' :
+
+
+
+
+    
+    
+    
+    
+    
+
+    
+
 
