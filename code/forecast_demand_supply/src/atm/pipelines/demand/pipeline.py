207b7fc0cb0844b95f273cc71a5e4d248909f49c
KrishnenduGhorui <77465776+KrishnenduGhorui@users.noreply.github.com>
2024-05-04 06:11:49 -0700

    Update pipeline.py

--- code/forecast_demand_supply/src/atm/pipelines/demand/pipeline.py
+++ code/forecast_demand_supply/src/atm/pipelines/demand/pipeline.py
@@ -66,6 +66,43 @@ def demand_data_pipeline (stack_flag, suffix, level):
                 )
                 ]
             )
+    elif(stack_flag==1):
+                return Pipeline(
+                    [
+                     node(
+                func=load_demand_data_queue,
+                inputs=[f'data _df_{level}',f'params:{suffix}' ],
+                outputs=f'demand_df_{suffix}_{level}',
+            ),
+                   node(
+                        func=train_test_splitting,
+                        inputs=[f'demand df_{suffix}_{level}',f'params : rule_type_list',f' params : Volume_train _start _date', f' params : run_date', f' params :{level}',f'params :demand_ split_ratio_{level}'],
+                        outputs=[f'train_start_date _{suffix}_{level}' ,f'train_end _date_{suffix}_{level}', f'test _start _date_{suffix}_{level}', f'test_end_date_{suffix}_{level}'],
+                   ),
+                    node(
+                        func=train_forecast_models,
+                        inputs=[f'demand_df_{suffix}_{level}', f'params : rule_type_list',f'train_start_date_{suffix}_{level}',f'train_end dste_{suffix}_{level}',f'test_start_date_{suffix}_{level}', f'test_end_date {suffix}_{level}', f'params : {level}_params_ets',f'params:{level}_params_sarima', f'params : perams_tes',f'params : params_grid_prophet' ,f'params :model_{level}'],
+                        outputs=f'best_models_{suffix}_{level}',
+                    node(
+                        func=holdout_metrics,
+                        inputs=[f'demand_df_{suffix}_{level}', f' best_models_{suffix}_{level}','returned_list',f'test_start_date_{suffix}_{level}', f'test_end_date_ {suffix}_{level}',f'params:model_{level}'],
+                        outputs=f'model_mape_dict_{suffix}_{level}'
+                        ),
+                    node(
+                        func=select_model_forecast,
+                        inputs=[f'params:{suffix}' , f'model_mape_dict_{suffix}_{level}',f'params : score_nodel_id', f'params : run_key ',f'params : output_project',f'params :output_db_name'
+                                ,f'params :output_metrics_{level}_tb_name', 'params : run_date '],
+                        outputs=f' rule_type_model_dict_{suffix}_{level}'
+                       ),
+                    node (
+                        func=forecasting_model_selection,
+                        inputs[f'params :{suffix}',f'params : {level}',f' best_models_{suffix}_{level}',f'rule_type_model_dict_{suffix}_{level}', f' demand_df_{suffix}_{level}',f'train_start_date_{suffiX}_{level}', f'train_end_date_{suffix}_{level}',f'test_start_date_{suffix}_{level}',f'test_end_date_ {suffix}_{level},f'params:
+                                {level}_forecast period',f'params:model_{level}',f'params :num_lags',f'params:score_model_id',f'params :run_key', 'params: run_date'],
+                        outputs=f'output_df_{suffix}_{level}'
+                        )
+                ]
+            )
+                        
                 
 
 
